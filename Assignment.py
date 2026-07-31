b56c54c191d42b03b760cc9e4b3ff07a53821099
akiolaSummayyah <akiolamodupe@gmail.com>
2026-07-31 22:12:00 +0400

    Addition of Homework subclass and inheritance from the parent class Assignment!

--- Assignment.py
+++ Assignment.py
@@ -14,6 +14,18 @@ print(home_work1.score)   # Output: 85.0
 print(home_work1.max_score)  # Output: 100.0
 print(home_work1.due_date)  # Output: 2023-09-15
 print(home_work1.type)    # Output: Homework
+class Home_work(Assignment):
+    def __init__(self, subject, title, score, max_score, due_date):
+        super().__init__(subject, title, score, max_score, due_date, "Homework") 
+Homework2=Home_work("Chemistry", "Isotopes Homework 2", 90, 100, "2023-09-22") 
+print(Homework2.subject)  # Output: chemistry
+print(Homework2.title)   # Output: Isotopes Homework 2
+print(Homework2.score)   # Output: 90.0
+print(Homework2.max_score)  # Output: 100.0
+print(Homework2.due_date)  # Output: 2023-09-22
+print(Homework2.type)    # Output: Homework
+                  
+
         
 
 
